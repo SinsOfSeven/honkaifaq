6ed97e3287964430667112ea1e1b8d95c1eac33a
Camelva <15131987+Camelva@users.noreply.github.com>
2022-12-17 18:52:28 +0200

    fix vue warnings in console

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -27,6 +27,14 @@ export default defineConfig({
         }
     },
 
+    vue: {
+        template: {
+            compilerOptions: {
+                isCustomElement: (tag) => mathCustomElements.includes(tag)
+            }
+        }
+    },
+
     async transformHead(ctx) {
         let description = ctx.siteData.description
 
@@ -132,3 +140,93 @@ function customBar() {
         }
     ]
 }
+
+const mathCustomElements = [
+    'math',
+    'maction',
+    'maligngroup',
+    'malignmark',
+    'menclose',
+    'merror',
+    'mfenced',
+    'mfrac',
+    'mi',
+    'mlongdiv',
+    'mmultiscripts',
+    'mn',
+    'mo',
+    'mover',
+    'mpadded',
+    'mphantom',
+    'mroot',
+    'mrow',
+    'ms',
+    'mscarries',
+    'mscarry',
+    'mscarries',
+    'msgroup',
+    'mstack',
+    'mlongdiv',
+    'msline',
+    'mstack',
+    'mspace',
+    'msqrt',
+    'msrow',
+    'mstack',
+    'mstack',
+    'mstyle',
+    'msub',
+    'msup',
+    'msubsup',
+    'mtable',
+    'mtd',
+    'mtext',
+    'mtr',
+    'munder',
+    'munderover',
+    'semantics',
+    'math',
+    'mi',
+    'mn',
+    'mo',
+    'ms',
+    'mspace',
+    'mtext',
+    'menclose',
+    'merror',
+    'mfenced',
+    'mfrac',
+    'mpadded',
+    'mphantom',
+    'mroot',
+    'mrow',
+    'msqrt',
+    'mstyle',
+    'mmultiscripts',
+    'mover',
+    'mprescripts',
+    'msub',
+    'msubsup',
+    'msup',
+    'munder',
+    'munderover',
+    'none',
+    'maligngroup',
+    'malignmark',
+    'mtable',
+    'mtd',
+    'mtr',
+    'mlongdiv',
+    'mscarries',
+    'mscarry',
+    'msgroup',
+    'msline',
+    'msrow',
+    'mstack',
+    'maction',
+    'semantics',
+    'annotation',
+    'annotation-xml',
+    'eq',
+    'eqn'
+]
